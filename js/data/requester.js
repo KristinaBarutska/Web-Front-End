30e33c94486831a966b499273ba073dc3891afae
KristinaBarutska <kristina.barutska@abv.bg>
2017-03-11 10:18:54 +0200

    Fixed registration bug

--- js/data/requester.js
+++ js/data/requester.js
@@ -1,12 +1,13 @@
 import { jqueryRequester } from "jquery-requester";
 
 var requester = (function() {
+
     const CURRENT_USER_ID = "current-user-id";
     const AUTH_TOKEN = "auth-token";
     const CURRENT_USER_NAME = "username";
-    const appId = "kid_rJWkfzw6";
-    const appSecret = "9861c3e41b4a41e4976d17e5c48f8e26";
-    const masterSecret = "039e3c57e4474187aa9c5b6d540c13e1";
+    const appId = "kid_ryOQxnRFl";
+    const appSecret = "cadb39eed8ef4d86bcb230484ccf0b80";
+    const masterSecret = "41da989e3d204611aa8411e752e50420";
     var authorizationString = `${appId}:${appSecret}`;
     authorizationString = btoa(authorizationString);
 
@@ -14,12 +15,13 @@ var requester = (function() {
     const url = "https://baas.kinvey.com";
     const getUrl = url + appId;
     const userUrl = url + `/user/${appId}`;
-    const loginUserUrl = userUrl + "login";
-    const logoutUserUrl = userUrl + "_logout";
+    const loginUserUrl = userUrl + "/login";
+    const logoutUserUrl = userUrl + "/_logout";
     const getRestaurantsUrl = url + `/appdata/${appId}/restaurants`;
 
     function registerUserRequest(username, password) {
-        const data = { username, password };
+        let favourites = [];
+        const data = { username, password, favourites };
         const headers = { Authorization: `Basic ${authorizationString}` };
         return jqueryRequester.post(userUrl, headers, data);
     }
@@ -29,6 +31,8 @@ var requester = (function() {
         const headers = { Authorization: `Basic ${authorizationString}` };
         return jqueryRequester.post(loginUserUrl, headers, data)
             .then((res) => {
+                console.log(res);
+
                 localStorage.setItem(CURRENT_USER_ID, res._id);
                 localStorage.setItem(AUTH_TOKEN, res._kmd.authtoken);
                 localStorage.setItem(CURRENT_USER_NAME, res.username);
@@ -37,10 +41,11 @@ var requester = (function() {
 
     function logoutUserRequest(username, password) {
         const data = { username, password };
-        const authtoken = localStorage.getItem(AUTH_TOKEN);
+        var authtoken = localStorage.getItem(AUTH_TOKEN);
         const headers = { Authorization: `Kinvey ${authtoken}` };
         return jqueryRequester.post(logoutUserUrl, headers, data)
             .then(() => {
+
                 localStorage.removeItem(CURRENT_USER_ID);
                 localStorage.removeItem(CURRENT_USER_NAME);
                 localStorage.removeItem(AUTH_TOKEN);
@@ -57,41 +62,16 @@ var requester = (function() {
         });
     }
 
-
     function getUserInfo() {
         const id = localStorage.getItem(CURRENT_USER_ID);
         const url = userUrl + `/${id}`;
         const authtoken = localStorage.getItem(AUTH_TOKEN);
         const headers = { Authorization: `Kinvey ${authtoken}` };
-
         return jqueryRequester.get(url, headers);
     }
 
-    function addRestaurantToFavourites(restaurantId) {
-        const userId = localStorage.getItem(CURRENT_USER_ID);
-        const authtoken = localStorage.getItem(AUTH_TOKEN);
-        const headers = { Authorization: `Kinvey ${authtoken}` };
-        const url = userUrl + `/${userId}`;
-        var newFavourites;
-
-        return getUserInfo()
-            .then((user) => {
-                var favourites = user.favourites;
-                newFavourites = favourites;
-                if (newFavourites.indexOf(restaurantId) < 0) {
-                    newFavourites.push(restaurantId);
-                } else {
-                    throw new Error("The place is already added to favourites.");
-                }
-            })
-            .then(() => {
-                var data = { favourites: newFavourites };
-                return jqueryRequester.put(url, headers, data);
-            });
-    }
-
     function getAllRestaurants() {
-        const authtoken = localStorage.getItem(AUTH_TOKEN);
+        var authtoken = localStorage.getItem(AUTH_TOKEN);
         const headers = { Authorization: `Kinvey ${authtoken}` };
         return jqueryRequester.get(getRestaurantsUrl, headers);
     }
@@ -102,6 +82,29 @@ var requester = (function() {
         const url = getRestaurantsUrl + `/${id}`;
         return jqueryRequester.get(url, headers);
     }
+
+    function addRestaurantToFavourites(restaurantId) {
+        const userId = localStorage.getItem(CURRENT_USER_ID);
+        const authtoken = localStorage.getItem(AUTH_TOKEN);
+        const headers = { Authorization: `Kinvey ${authtoken}` };
+        const url = userUrl + `/${userId}`;
+        let newFavourites;
+        return getUserInfo()
+            .then((user) => {
+                let favourites = user.favourites;
+                newFavourites = favourites;
+                if (newFavourites.indexOf(restaurantId) < 0) {
+                    newFavourites.push(restaurantId);
+                } else {
+                    throw new Error("The place is already added to favourites.");
+                }
+            })
+            .then(() => {
+                let data = { favourites: newFavourites };
+                return jqueryRequester.put(url, headers, data);
+            });
+    }
+
 
     return {
         registerUser: registerUserRequest,
