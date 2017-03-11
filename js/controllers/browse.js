adc66bd077d50d7e0b3f527334dc99383364d636
KristinaBarutska <kristina.barutska@abv.bg>
2017-03-11 14:34:37 +0200

    Add comments

--- js/controllers/browse.js
+++ js/controllers/browse.js
@@ -4,8 +4,8 @@
  let browse = (function() {
 
      function getBrowsing(context) {
-         var data;
-         var templateListFunc;
+         let data;
+         let templateListFunc;
          requester.getAllRestaurants()
              .then((result) => {
                  data = result;
@@ -18,27 +18,71 @@
                      .then((listFunc) => {
                          templateListFunc = listFunc;
                          $("#restaurants-list").html(templateListFunc({ data }));
-
-                         $(".add-to-favourites").on("click", function(ev) {
-                             const parent = $(ev.target).parents(".restaurant");
-                             const id = parent.attr("data-id");
-                             console.log(id);
-                             requester.addRestaurantToFavourites(id)
-                                 .then(() => {
-                                     toastr.success("Added to favourites!");
-                                 })
-                                 .catch((ex) => {
-                                     if (ex.message.indexOf("is already added to favourites") >= 0) {
-                                         toastr.success("The place is already added to favourites.");
-                                     } else {
-                                         toastr.error("An error occured and the place is not added to favourites.");
-                                     }
-                                 });
-
-                             ev.preventDefault();
-                             return false;
-                         });
                      });
+
+                 $("#browse-container").on("click", ".add-to-favourites", function(ev) {
+                     const parent = $(ev.target).parents(".restaurant");
+                     const id = parent.attr("data-id");
+                     console.log(id);
+                     requester.addRestaurantToFavourites(id)
+                         .then(() => {
+                             toastr.success("Added to favourites!");
+                         })
+                         .catch((ex) => {
+                             if (ex.message.indexOf("is already added to favourites") >= 0) {
+                                 toastr.success("The place is already added to favourites.");
+                             } else {
+                                 toastr.error("An error occured and the place is not added to favourites.");
+                             }
+                         });
+
+                     ev.preventDefault();
+                     return false;
+                 });
+
+                 let description;
+                 let parent;
+                 $("#browse-container").on("click", ".btn-info", function(ev) {
+                     parent = $(ev.target).parents('.restaurant');
+
+                     if (parent.children().children('.desc').children().length == 0) {
+                         let id = parent.attr("data-id");
+                         requester.getRestaurantById(id)
+                             .then((res) => {
+                                 description = res.description;
+                                 return templates.get('description');
+                             })
+                             .then((templateFunc) => {
+                                 parent.children().children('.desc').append(templateFunc(description));
+                             })
+                     } else {
+                         parent.children().children('.desc').children().remove('#restaurant-description');
+                     }
+                     ev.preventDefault();
+                     return false;
+                 });
+
+                 let comment;
+                 let parentComment;
+                 $("#browse-container").on("click", ".btn-comments", function(ev) {
+                     parentComment = $(ev.target).parents('.restaurant');
+
+                     if (parentComment.children().children('.comments').children().length == 0) {
+                         var id = parentComment.attr('data-id');
+                         requester.getRestaurantById(id)
+                             .then((res) => {
+                                 comment = res.comments;
+                                 return templates.get('comment');
+                             })
+                             .then((templateFunc) => {
+                                 parentComment.children().children('.comments').append(templateFunc(comment));
+                             })
+                     } else {
+                         parentComment.children().children('.comments').children().remove('.restaurant-comment');
+                     }
+                     ev.preventDefault();
+                     return false;
+                 });
 
                  addAutocomplete("type", "type", data);
                  addAutocomplete("city", "city", data);
@@ -60,16 +104,16 @@
 
                      requester.getAllRestaurants()
                          .then((restaurants) => {
-                             var sortedRestaurants = [];
+                             let sortedRestaurants = [];
                              restaurants.forEach((restaurant) => {
-                                 var answersConditions = true;
-                                 for (var property in options) {
+                                 let answersConditions = true;
+                                 for (let property in options) {
                                      if (options[property] === "") {
                                          continue;
                                      }
 
                                      if (Array.isArray(restaurant[property])) {
-                                         var hasMatch = restaurant[property].some((element) => element.toLowerCase() === options[property]);
+                                         let hasMatch = restaurant[property].some((element) => element.toLowerCase() === options[property]);
                                          if (hasMatch) {
                                              continue;
                                          } else {
@@ -97,8 +141,8 @@
                      return false;
                  });
              })
-             .catch((e) => {
-                 console.log(e);
+             .catch(() => {
+                 console.log();
                  templates.get("not-logged-in")
                      .then((templateFunc) => {
                          context.$element().html(templateFunc());
@@ -109,7 +153,7 @@
 
      function addAutocomplete(property, field, data, isInArray) {
          isInArray = isInArray || false;
-         var options = [];
+         let options = [];
          if (isInArray) {
              data.map(r => r[property])
                  .forEach((array) => {
